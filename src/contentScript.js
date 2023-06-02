3c1f2c81f6c607749a01e9a5e90d7068b5d7965b
Benjamin Bigot <benjamin.bigot@gmail.com>
2023-06-02 12:04:19 +0200

    Access to myludo game page from bga end game page

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,27 +1,36 @@
 var rematchButton = document.getElementById("rematch");
 
-if(rematchButton !== null )
-{
+if (rematchButton !== null) {
     var displayStyle = window.getComputedStyle(rematchButton).display;
-    
-    if(displayStyle !== 'none' || displayStyle !== 'hide' )
-    {      
+
+    if (displayStyle !== 'none' || displayStyle !== 'hide') {
         var bgaButtonBar = document.getElementsByClassName("bgabuttonbar");
 
-        if(bgaButtonBar !== null && bgaButtonBar.length > 0)
-        {
-            var link = document.createElement("a");
-            link.href = "https://www.myludo.fr";
-            link.textContent = "Enregistrer la partie sur MyLudo";
-            link.target = "_blank"
-            link.classList.add("bgabutton");
-            link.classList.add("bgabutton_gray");
-            link.id = "myludo_browserextension";
-            link.style = "display: inline;"
+        if (bgaButtonBar !== null && bgaButtonBar.length > 0) {
+            var game = document.getElementById('table_name');
 
-            Array.from(bgaButtonBar).forEach((element)=>{
-                element.appendChild(link);
-            });
+            if (game !== null) {
+                const jsonFileURL = chrome.runtime.getURL('locales/fr.json');
+
+                // Chargez le fichier JSON
+                fetch(jsonFileURL).then(response => response.json())
+                    .then(data => {
+                        const myludoId = data[game.innerText];
+
+                        var link = document.createElement("a");
+                        link.href = `https://www.myludo.fr/#!/game/${myludoId}`;
+                        link.textContent = "Enregistrer la partie sur MyLudo";
+                        link.target = "_blank"
+                        link.classList.add("bgabutton");
+                        link.classList.add("bgabutton_gray");
+                        link.id = "myludo_browserextension";
+                        link.style = "display: inline;"
+
+                        Array.from(bgaButtonBar).forEach((element) => {
+                            element.appendChild(link);
+                        });
+                    });
+            }
         }
     }
 }
