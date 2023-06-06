28e7719972c23f7f97bc00ffd35c1169b6309826
Benjamin Bigot <benjamin.bigot@gmail.com>
2023-06-06 14:11:48 +0200

    add score and player in myludo

--- src/js/myLudo.js
+++ src/js/myLudo.js
@@ -6,7 +6,7 @@ chrome.runtime.onMessage.addListener(
     }
 );
 
-let intervalID = setInterval(patch, 250);
+const intervalID = setInterval(patch, 250);
 
 function patch() {
     console.log("myLudo.js");
@@ -18,12 +18,33 @@ function patch() {
     if (url.includes("bga2myludo") && addPlayButton.length > 0) {
         clearInterval(intervalID);
 
-        let parameters = {};
+        let parameters = [];
         url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (match, name, value) {
-            if (name != "bga2myludo") parameters[name] = decodeURIComponent(value);
+            if (name != "bga2myludo") {
+                parameters.push({ player: name, score: decodeURIComponent(value) });
+            }
         });
 
         addPlayButton.item(0).click();
+
+        const intervalPopupID = setInterval(() => {
+            const addPlayerButton = document.getElementsByClassName('btn-add-player');
+
+            if (addPlayerButton.length > 0) {
+
+                parameters.forEach((elt, index) => {
+                    addPlayerButton.item(0).click();
+
+                    const currentPlayer = document.getElementById(`name-${index}`);
+                    const currentScore = document.getElementById(`score-${index}`);
+
+                    currentPlayer.value = elt.player;
+                    currentScore.value = elt.score;
+                });
+
+                clearInterval(intervalPopupID);
+            }
+        }, 250);
 
         window.location.href = window.location.href.replace(/[?&][^=]+=[^&]+/g, "");
     }
