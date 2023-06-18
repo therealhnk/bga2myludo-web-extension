f7156ef5970155d476d6472c27ab41a44f1368f5
Benjamin Bigot <benjamin.bigot@gmail.com>
2023-06-18 22:44:05 +0200

    call to api tableinfos to get score and players

--- src/js/boardGameArena.js
+++ src/js/boardGameArena.js
@@ -5,34 +5,43 @@ function patch() {
     const regexpEndGamePage = /^https:\/\/boardgamearena\.com\/(?!(?:.*\btable\?\b.*))\S*table=\d+$/;
 
 
-    if (regexTablePage.test(window.location.href)) {
-        patchTablePage();
-    }
-
-    if (regexpEndGamePage.test(window.location.href)) {
-        patchEndGamePage();
+    if (regexTablePage.test(window.location.href) || regexpEndGamePage.test(window.location.href)) {
+        patchTablePage()
     }
 
     return;
 }
 
-function patchTablePage() {
+async function patchTablePage() {
     if (document.getElementById("myludo_browserextension") !== null) return;
-
-    const scores = document.getElementsByClassName('score-entry');
-
-    if (scores.length === 0) return;
 
     const rematchButton = document.getElementById("rematch");
 
     if (rematchButton !== null) {
         const displayStyle = window.getComputedStyle(rematchButton).display;
 
-        if (displayStyle !== 'none' || displayStyle !== 'hide') {
+        if (displayStyle !== 'none' && displayStyle !== 'hide') {
             const bgaButtonBar = document.getElementsByClassName("bgabuttonbar");
 
             if (bgaButtonBar !== null && bgaButtonBar.length > 0) {
-                const gameName = getGameName();
+
+                const queryString = window.location.search;
+                const urlParams = new URLSearchParams(queryString);
+                const tableId = urlParams.get("table");
+
+
+                const result = await fetch(`https://boardgamearena.com/account/account/getRequestToken.html?bgachromeext`)
+                    .then(response => response.json())
+                    .then(data => { return data; });
+
+                const headers = {};
+                headers["x-request-token"] = result.data.request_token;
+
+                const resultTable = await fetch(`https://boardgamearena.com/table/table/tableinfos.html?id=${tableId}`, { headers })
+                    .then(response => response.json())
+                    .then(data => { return data; });
+
+                const gameName = resultTable.data.result.game_name;
 
                 if (gameName !== null) {
                     const jsonFileURL = chrome.runtime.getURL('data/table.json');
@@ -50,14 +59,17 @@ function patchTablePage() {
 
                             const result = [];
 
-                            if (scores !== null) {
-                                for (let elt of scores) {
-                                    const playerName = elt.getElementsByClassName('name')[0].innerText.trim();
-                                    const playerScore = elt.getElementsByClassName('score')[0].innerText.trim();
+                            resultTable.data.result.player.forEach((item) => {
+                                result.push(`${item.name === connectedUser ? "Moi" : item.name}=${item.score}`);
+                                console.log(`${item.name} - place : ${item.gamerank} - score : ${item.score}`);
+                            });
 
-                                    result.push(`${playerName === connectedUser ? "Moi" : playerName}=${playerScore}`);
-                                }
-                            }
+
+                            console.log("date de fin : " + resultTable.data.result.time_end);
+                            console.log("coop : " + resultTable.data.result.is_coop);
+                            // abandon_by_decision => partie pas terminé
+                            // normal_end => partie terminé
+                            console.log("endgame_reason : " + resultTable.data.result.endgame_reason);
 
                             const link = document.createElement("a");
                             link.href = `https://www.myludo.fr/#!/game/${myludoId}?bga2myludo=1&${result.join("&")}`;
@@ -76,82 +88,4 @@ function patchTablePage() {
             }
         }
     }
-}
-
-function patchEndGamePage() {
-    if (document.getElementById("myludo_browserextension") !== null) return;
-
-    const scores = document.getElementsByClassName('score-entry');
-
-    if (scores.length === 0) return;
-
-    const rematchButton = document.getElementById("createNew_btn");
-
-    if (rematchButton !== null) {
-        const displayStyle = window.getComputedStyle(rematchButton).display;
-
-        if (displayStyle !== 'none' || displayStyle !== 'hide') {
-            const bgaButtonBar = document.getElementById("generalactions");
-
-            if (bgaButtonBar !== null) {
-                const gameNameMatch = window.location.href.match(/\/([^/?]+)\?/)
-                const gameName = gameNameMatch ? gameNameMatch[1] : null;
-
-                if (gameName !== null) {
-                    const jsonFileURL = chrome.runtime.getURL('data/table.json');
-
-                    fetch(jsonFileURL).then(response => response.json())
-                        .then(data => {
-                            const myludoId = data[gameName];
-
-                            if (!myludoId) {
-                                console.log(`[bga2myludo]missing myludo id for ${gameName}`);
-                                return;
-                            }
-
-                            const connectedUser = document.getElementById('connected_username').innerText;
-
-                            const result = [];
-
-                            if (scores !== null) {
-                                for (let elt of scores) {
-                                    const playerName = elt.getElementsByClassName('name')[0].innerText.trim();
-                                    const playerScore = elt.getElementsByClassName('score')[0].innerHTML.split('<')[0].trim();
-
-                                    result.push(`${playerName === connectedUser ? "Moi" : playerName}=${playerScore}`);
-                                }
-                            }
-
-                            const link = document.createElement("a");
-                            link.href = `https://www.myludo.fr/#!/game/${myludoId}?bga2myludo=1&${result.join("&")}`;
-                            link.textContent = "Enregistrer la partie sur MyLudo";
-                            link.target = "_blank"
-                            link.classList.add("action-button");
-                            link.classList.add("bgabutton");
-                            link.classList.add("bgabutton_darkgray");
-                            link.id = "myludo_browserextension";
-                            link.style = "display: inline;"
-
-                            bgaButtonBar.appendChild(link);
-                        });
-                }
-            }
-        }
-    }
-}
-
-function getGameName() {
-    const element = document.getElementById("table-module");
-    const classValue = element.getAttribute("class");
-    const classes = classValue.split(" ");
-    const searchingTag = "tablewithgame_";
-
-    for (var i = 0; i < classes.length; i++) {
-        if (classes[i].indexOf(searchingTag) !== -1) {
-            var termIndex = classes[i].indexOf(searchingTag) + searchingTag.length;
-            return classes[i].substring(termIndex);
-        }
-    }
-
-    return null;
 }
