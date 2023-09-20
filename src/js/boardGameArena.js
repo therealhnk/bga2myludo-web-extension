45f1f22e2175e84bf1db5c75976646dc5f0f0344
Benjamin Bigot <benjamin.bigot@gmail.com>
2023-09-20 14:44:48 +0200

    fix : remove unecessary try catch

--- src/js/boardGameArena.js
+++ src/js/boardGameArena.js
@@ -1,173 +1,171 @@
-try {
-    const myLudoEltId = "myludo_browserextension";
+const myLudoEltId = "myludo_browserextension";
 
-    patch();
+patch();
 
-    async function patch() {
-        const regexTablePage = /^https:\/\/boardgamearena\.com\/table\?table=\d+#?$/;
-        const regexpEndGamePage = /^https:\/\/boardgamearena\.com\/.*\btable=\d+.*$/;
+async function patch() {
+    const regexTablePage = /^https:\/\/boardgamearena\.com\/table\?table=\d+#?$/;
+    const regexpEndGamePage = /^https:\/\/boardgamearena\.com\/.*\btable=\d+.*$/;
 
 
-        if (regexTablePage.test(window.location.href)) {
-            await patchTablePage();
-        } else if (regexpEndGamePage.test(window.location.href)) {
-            await patchEndGamePage();
-        }
-
-        setTimeout(() => patch(), 500)
+    if (regexTablePage.test(window.location.href)) {
+        await patchTablePage();
+    } else if (regexpEndGamePage.test(window.location.href)) {
+        await patchEndGamePage();
     }
 
-    async function patchTablePage() {
-        if (document.getElementById(myLudoEltId) !== null) return;
+    setTimeout(() => patch(), 500)
+}
 
-        const reviewgameButton = document.getElementById("reviewgame");
+async function patchTablePage() {
+    if (document.getElementById(myLudoEltId) !== null) return;
 
-        if (reviewgameButton !== null) {
-            const displayStyle = window.getComputedStyle(reviewgameButton).display;
+    const reviewgameButton = document.getElementById("reviewgame");
 
-            if (displayStyle !== 'none' && displayStyle !== 'hide') {
-                const bgaButtonBar = document.getElementsByClassName("bgabuttonbar");
+    if (reviewgameButton !== null) {
+        const displayStyle = window.getComputedStyle(reviewgameButton).display;
 
-                if (bgaButtonBar !== null && bgaButtonBar.length > 0) {
-                    await getMyLudoLink().then(link => {
-                        if (link === null) {
-                            addMyLudoField();
-                        }
-                        else {
-                            Array.from(bgaButtonBar).forEach((element) => {
-                                element.appendChild(link);
-                            });
-                        }
-                    });
-                }
-            }
-        }
-    }
+        if (displayStyle !== 'none' && displayStyle !== 'hide') {
+            const bgaButtonBar = document.getElementsByClassName("bgabuttonbar");
 
-    async function patchEndGamePage() {
-        if (document.getElementById(myLudoEltId) !== null) return;
-
-        const rematchButton = document.getElementById("createNew_btn");
-
-        if (rematchButton !== null) {
-            const displayStyle = window.getComputedStyle(rematchButton).display;
-
-            if (displayStyle !== 'none' && displayStyle !== 'hide') {
-                const bgaButtonBar = document.getElementById("generalactions");
-
-                if (bgaButtonBar !== null) {
-                    await getMyLudoLink().then(link => {
-                        if (link === null) {
-                            addMyLudoField();
-                        }
-                        else {
-                            bgaButtonBar.appendChild(link);
-                        }
-                    });
-                }
-            }
-        }
-    }
-
-    function addMyLudoField() {
-        const input = document.createElement("input");
-        input.setAttribute("type", "hidden");
-        input.setAttribute("id", myLudoEltId);
-
-        document.body.appendChild(input);
-    }
-
-    async function getMyLudoLink() {
-        const queryString = window.location.search;
-        const urlParams = new URLSearchParams(queryString);
-        const tableId = urlParams.get("table");
-
-        const requestToken = getRequestToken();
-
-        if (requestToken === null) return null;
-
-        const headers = {};
-        headers["x-request-token"] = requestToken
-
-        const resultTable = await fetch(`https://boardgamearena.com/table/table/tableinfos.html?id=${tableId}`, { headers })
-            .then(response => response.json())
-            .then(data => { return data; });
-
-        const gameName = resultTable.data.result.game_name;
-
-        if (gameName !== null) {
-            const jsonFileURL = chrome.runtime.getURL('data/table.json');
-
-            return await fetch(jsonFileURL)
-                .then(response => response.json())
-                .then(async data => {
-                    const myludoId = data[gameName];
-                    let href = '';
-
-                    if (!myludoId) {
-                        href = `https://www.myludo.fr/#!/search/${gameName}`;
+            if (bgaButtonBar !== null && bgaButtonBar.length > 0) {
+                await getMyLudoLink().then(link => {
+                    if (link === null) {
+                        addMyLudoField();
                     }
                     else {
-                        const result = {
-                            table: { id: tableId },
-                            players: [],
-                            end: '',
-                            isCooperative: false,
-                            isSolo: false,
-                            isAbandoned: false
-                        };
-
-                        const resultUser = await fetch(`https://boardgamearena.com/my?who`, { headers })
-                            .then(response => response.json())
-                            .then(data => { return data; });
-
-                        const connectedUser = resultUser.n; // username 
-
-                        resultTable.data.result.player.forEach((item) => {
-                            result.players.push({
-                                name: item.name === connectedUser ? "Moi" : item.name,
-                                score: item.score,
-                                rank: item.gamerank
-                            });
+                        Array.from(bgaButtonBar).forEach((element) => {
+                            element.appendChild(link);
                         });
-
-                        result.end = resultTable.data.result.time_end;
-                        result.isCooperative = resultTable.data.result.is_coop;
-                        result.isSolo = resultTable.data.result.is_solo;
-                        result.isAbandoned = resultTable.data.result.endgame_reason !== 'normal_end';
-
-                        const json = JSON.stringify(result);
-                        const data = btoa(json);
-
-                        href = `https://www.myludo.fr/#!/game/${myludoId}?bga2myludo=1&bga2myludo_data=${data}`
                     }
-
-                    const link = document.createElement("a");
-                    link.href = href;
-                    link.textContent = "Enregistrer la partie sur Myludo";
-                    link.target = "_blank"
-                    link.classList.add("action-button");
-                    link.classList.add("bgabutton");
-                    link.classList.add("bgabutton_darkgray");
-                    link.id = "myludo_browserextension";
-                    link.style = "display: inline;"
-
-                    return link;
                 });
+            }
         }
+    }
+}
 
+async function patchEndGamePage() {
+    if (document.getElementById(myLudoEltId) !== null) return;
+
+    const rematchButton = document.getElementById("createNew_btn");
+
+    if (rematchButton !== null) {
+        const displayStyle = window.getComputedStyle(rematchButton).display;
+
+        if (displayStyle !== 'none' && displayStyle !== 'hide') {
+            const bgaButtonBar = document.getElementById("generalactions");
+
+            if (bgaButtonBar !== null) {
+                await getMyLudoLink().then(link => {
+                    if (link === null) {
+                        addMyLudoField();
+                    }
+                    else {
+                        bgaButtonBar.appendChild(link);
+                    }
+                });
+            }
+        }
+    }
+}
+
+function addMyLudoField() {
+    const input = document.createElement("input");
+    input.setAttribute("type", "hidden");
+    input.setAttribute("id", myLudoEltId);
+
+    document.body.appendChild(input);
+}
+
+async function getMyLudoLink() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const tableId = urlParams.get("table");
+
+    const requestToken = getRequestToken();
+
+    if (requestToken === null) return null;
+
+    const headers = {};
+    headers["x-request-token"] = requestToken
+
+    const resultTable = await fetch(`https://boardgamearena.com/table/table/tableinfos.html?id=${tableId}`, { headers })
+        .then(response => response.json())
+        .then(data => { return data; });
+
+    const gameName = resultTable.data.result.game_name;
+
+    if (gameName !== null) {
+        const jsonFileURL = chrome.runtime.getURL('data/table.json');
+
+        return await fetch(jsonFileURL)
+            .then(response => response.json())
+            .then(async data => {
+                const myludoId = data[gameName];
+                let href = '';
+
+                if (!myludoId) {
+                    href = `https://www.myludo.fr/#!/search/${gameName}`;
+                }
+                else {
+                    const result = {
+                        table: { id: tableId },
+                        players: [],
+                        end: '',
+                        isCooperative: false,
+                        isSolo: false,
+                        isAbandoned: false
+                    };
+
+                    const resultUser = await fetch(`https://boardgamearena.com/my?who`, { headers })
+                        .then(response => response.json())
+                        .then(data => { return data; });
+
+                    const connectedUser = resultUser.n; // username 
+
+                    resultTable.data.result.player.forEach((item) => {
+                        result.players.push({
+                            name: item.name === connectedUser ? "Moi" : item.name,
+                            score: item.score,
+                            rank: item.gamerank
+                        });
+                    });
+
+                    result.end = resultTable.data.result.time_end;
+                    result.isCooperative = resultTable.data.result.is_coop;
+                    result.isSolo = resultTable.data.result.is_solo;
+                    result.isAbandoned = resultTable.data.result.endgame_reason !== 'normal_end';
+
+                    const json = JSON.stringify(result);
+                    const data = btoa(json);
+
+                    href = `https://www.myludo.fr/#!/game/${myludoId}?bga2myludo=1&bga2myludo_data=${data}`
+                }
+
+                const link = document.createElement("a");
+                link.href = href;
+                link.textContent = "Enregistrer la partie sur Myludo";
+                link.target = "_blank"
+                link.classList.add("action-button");
+                link.classList.add("bgabutton");
+                link.classList.add("bgabutton_darkgray");
+                link.id = "myludo_browserextension";
+                link.style = "display: inline;"
+
+                return link;
+            });
+    }
+
+    return null;
+}
+
+function getRequestToken() {
+    const regex = /requestToken:\s+'([^']+)'/;
+
+    const requestToken = document.head.outerHTML.match(regex);
+
+    if (requestToken) {
+        return requestToken[1];
+    } else {
         return null;
     }
-
-    function getRequestToken() {
-        const regex = /requestToken:\s+'([^']+)'/;
-
-        const requestToken = document.head.outerHTML.match(regex);
-
-        if (requestToken) {
-            return requestToken[1];
-        } else {
-            return null;
-        }
-    }
-} catch (Exception) { };
+}
--- src/js/myLudo.js
+++ src/js/myLudo.js
@@ -1,196 +1,194 @@
-try {
-    const intervalID = setInterval(patch, 125);
+const intervalID = setInterval(patch, 125);
 
-    async function patch() {
-        if (!window.location.href.startsWith("https://www.myludo.fr/#!/game/")) {
-            clearInterval(intervalID);
-            return;
-        }
+async function patch() {
+    if (!window.location.href.startsWith("https://www.myludo.fr/#!/game/")) {
+        clearInterval(intervalID);
+        return;
+    }
 
-        const addPlayButton = document.getElementsByClassName('btn-play-open');
+    const addPlayButton = document.getElementsByClassName('btn-play-open');
 
-        if (window.location.href.includes("bga2myludo") && addPlayButton.length > 0) {
-            clearInterval(intervalID);
+    if (window.location.href.includes("bga2myludo") && addPlayButton.length > 0) {
+        clearInterval(intervalID);
 
-            const data = getDataFromBGA();
+        const data = getDataFromBGA();
 
-            await loadPlays((plays => {
-                const hasBeenPlayed = hasBeenAlreadyPlayed(data, plays);
+        await loadPlays((plays => {
+            const hasBeenPlayed = hasBeenAlreadyPlayed(data, plays);
 
-                addPlayButton.item(0).click();
+            addPlayButton.item(0).click();
 
-                const intervalPopupID = setInterval(() => {
-                    const addPlayerButton = document.getElementsByClassName('btn-add-player');
+            const intervalPopupID = setInterval(() => {
+                const addPlayerButton = document.getElementsByClassName('btn-add-player');
 
-                    if (addPlayerButton.length > 0) {
-                        clearInterval(intervalPopupID);
+                if (addPlayerButton.length > 0) {
+                    clearInterval(intervalPopupID);
 
-                        if (hasBeenPlayed) addWarning();
+                    if (hasBeenPlayed) addWarning();
 
-                        document.getElementById('online').click();
+                    document.getElementById('online').click();
 
-                        if (data.isCooperative) document.getElementById('coop').click();
-                        if (data.isSolo) document.getElementById('solo').click();
-                        if (data.isAbandoned) document.getElementById('incomplete').click();
+                    if (data.isCooperative) document.getElementById('coop').click();
+                    if (data.isSolo) document.getElementById('solo').click();
+                    if (data.isAbandoned) document.getElementById('incomplete').click();
 
-                        if (data.isSolo) {
-                            document.querySelector(`label[for="name-0"]`).click();
-                            document.getElementById(`name-0`).value = data.players[0].name;
+                    if (data.isSolo) {
+                        document.querySelector(`label[for="name-0"]`).click();
+                        document.getElementById(`name-0`).value = data.players[0].name;
 
-                            document.querySelector(`label[for="score-0"]`).click();
-                            document.getElementById(`score-0`).value = data.players[0].score;
-                        }
-                        else {
-                            data.players.forEach((elt, index) => {
-                                addPlayerButton.item(0).click();
-
-                                document.querySelector(`label[for="name-${index}"]`).click();
-
-                                const currentPlayer = document.getElementById(`name-${index}`);
-                                currentPlayer.value = elt.name;
-
-                                if (elt.rank === "1") {
-                                    currentPlayer.closest('.card-content').getElementsByClassName('btn-winner-player')[0].click();
-                                }
-
-                                document.querySelector(`label[for="score-${index}"]`).click();
-                                document.getElementById(`score-${index}`).value = elt.score;
-                            });
-                        }
-
-                        document.getElementById(`date`).value = new Date(data.end).toISOString().split('T')[0];
-
-                        document.querySelector(`label[for="location"]`).click();
-                        document.getElementById(`location`).value = "Board Game Arena";
-
-                        document.querySelector(`label[for="message"]`).click();
-                        document.getElementById(`message`).value = `Table : https://www.boardgamearena.com/table?table=${data.table.id}\r\n\r\nCréé à l'aide de BGA2Myludo`;
-
-                        const modalContent = document.querySelector("#form-play .modal-content");
-                        modalContent.scrollTop = modalContent.scrollHeight;
+                        document.querySelector(`label[for="score-0"]`).click();
+                        document.getElementById(`score-0`).value = data.players[0].score;
                     }
-                }, 500);
+                    else {
+                        data.players.forEach((elt, index) => {
+                            addPlayerButton.item(0).click();
 
+                            document.querySelector(`label[for="name-${index}"]`).click();
 
-            }));
-        }
-    }
+                            const currentPlayer = document.getElementById(`name-${index}`);
+                            currentPlayer.value = elt.name;
 
-    function getDataFromBGA() {
-        const urlParams = new URL(`https://fake.com?${window.location.href.split("?")[1]}`).searchParams;
-        const json = atob(urlParams.get("bga2myludo_data"));
+                            if (elt.rank === "1") {
+                                currentPlayer.closest('.card-content').getElementsByClassName('btn-winner-player')[0].click();
+                            }
 
-        window.location.href = window.location.href.replace(/[?&][^=]+=[^&]+/g, "");
-
-        return JSON.parse(json);
-    }
-
-    async function loadPlays(callback) {
-        const plays = [];
-
-        const playsTab = document.querySelector('a[href="#plays"]');
-
-        if (playsTab) {
-            playsTab.click();
-
-            const intervalLoadPlaysID = setInterval(() => {
-                const loadingPlays = document.querySelector('#plays .progress');
-
-                if (!loadingPlays) {
-                    clearInterval(intervalLoadPlaysID);
-
-                    const playsContent = document.querySelectorAll('#plays .game-play');
-
-                    playsContent.forEach((currentPlay) => {
-                        const play = {
-                            end: convertToDate(currentPlay.querySelector('h4').textContent),
-                            players: []
-                        }
-
-                        currentPlay.querySelectorAll('.play-player').forEach((elt) => {
-                            const counter = elt.querySelector('.counter');
-                            play.players.push({
-                                name: elt.getAttribute('title'),
-                                score: counter ? counter.textContent : null
-                            })
+                            document.querySelector(`label[for="score-${index}"]`).click();
+                            document.getElementById(`score-${index}`).value = elt.score;
                         });
+                    }
 
-                        plays.push(play);
+                    document.getElementById(`date`).value = new Date(data.end).toISOString().split('T')[0];
+
+                    document.querySelector(`label[for="location"]`).click();
+                    document.getElementById(`location`).value = "Board Game Arena";
+
+                    document.querySelector(`label[for="message"]`).click();
+                    document.getElementById(`message`).value = `Table : https://www.boardgamearena.com/table?table=${data.table.id}\r\n\r\nCréé à l'aide de BGA2Myludo`;
+
+                    const modalContent = document.querySelector("#form-play .modal-content");
+                    modalContent.scrollTop = modalContent.scrollHeight;
+                }
+            }, 500);
+
+
+        }));
+    }
+}
+
+function getDataFromBGA() {
+    const urlParams = new URL(`https://fake.com?${window.location.href.split("?")[1]}`).searchParams;
+    const json = atob(urlParams.get("bga2myludo_data"));
+
+    window.location.href = window.location.href.replace(/[?&][^=]+=[^&]+/g, "");
+
+    return JSON.parse(json);
+}
+
+async function loadPlays(callback) {
+    const plays = [];
+
+    const playsTab = document.querySelector('a[href="#plays"]');
+
+    if (playsTab) {
+        playsTab.click();
+
+        const intervalLoadPlaysID = setInterval(() => {
+            const loadingPlays = document.querySelector('#plays .progress');
+
+            if (!loadingPlays) {
+                clearInterval(intervalLoadPlaysID);
+
+                const playsContent = document.querySelectorAll('#plays .game-play');
+
+                playsContent.forEach((currentPlay) => {
+                    const play = {
+                        end: convertToDate(currentPlay.querySelector('h4').textContent),
+                        players: []
+                    }
+
+                    currentPlay.querySelectorAll('.play-player').forEach((elt) => {
+                        const counter = elt.querySelector('.counter');
+                        play.players.push({
+                            name: elt.getAttribute('title'),
+                            score: counter.textContent
+                        })
                     });
 
-                    callback(plays);
-                }
-            }, 250);
-        }
-        else {
-            callback(plays);
-        }
-    }
+                    plays.push(play);
+                });
 
-    function convertToDate(text) {
-        const monthText = ["janv", "févr", "mars", "avr", "mai", "juin", "juill", "août", "sept", "oct", "nov", "déc"];
-        let result = new Date(1900, 0, 1);
-
-        text.replace(/(\d+) ([\p{L}\s]+) (\d+)/u, (match, day, month, year) => {
-            const monthIndex = monthText.indexOf(month);
-
-            if (monthIndex >= 0) {
-                result = new Date(
-                    year,
-                    monthIndex,
-                    day
-                );
+                callback(plays);
             }
-        });
-
-        return result;
+        }, 250);
     }
-
-    function trunkDateToDay(dateString) {
-        return new Date(new Date(dateString).toDateString());
+    else {
+        callback(plays);
     }
+}
 
-    function hasBeenAlreadyPlayed(currentPlay, plays) {
-        const currentPlayersFootPrint = JSON.stringify(currentPlay.players.map(o => { return { name: o.name, score: o.score }; }));
+function convertToDate(text) {
+    const monthText = ["janv", "févr", "mars", "avr", "mai", "juin", "juill", "août", "sept", "oct", "nov", "déc"];
+    let result = new Date(1900, 0, 1);
 
-        return plays.some(o =>
-            trunkDateToDay(o.end).getTime() === trunkDateToDay(currentPlay.end).getTime() &&
-            JSON.stringify(o.players) === currentPlayersFootPrint
-        );
-    }
+    text.replace(/(\d+) ([\p{L}\s]+) (\d+)/u, (match, day, month, year) => {
+        const monthIndex = monthText.indexOf(month);
 
-    function addWarning() {
-        const divider = document.createElement("div");
-        divider.classList.add("divider");
+        if (monthIndex >= 0) {
+            result = new Date(
+                year,
+                monthIndex,
+                day
+            );
+        }
+    });
 
-        const row = document.createElement("div");
-        row.classList.add("row");
-        row.classList.add("extra-margin-top");
+    return result;
+}
 
-        const warning = document.createElement("div");
-        warning.classList.add("col");
-        warning.classList.add("s12");
+function trunkDateToDay(dateString) {
+    return new Date(new Date(dateString).toDateString());
+}
 
-        const label = document.createElement("div");
-        label.textContent = "BGA2Myludo";
-        label.style = "padding-left:12px; font-size: .8rem; color: #9e9e9e;";
+function hasBeenAlreadyPlayed(currentPlay, plays) {
+    const currentPlayersFootPrint = JSON.stringify(currentPlay.players.map(o => { return { name: o.name, score: o.score }; }));
 
-        const warningIcon = document.createElement("i");
-        warningIcon.classList.add("material-icons");
-        warningIcon.classList.add("orange-text");
-        warningIcon.classList.add("middle");
-        warningIcon.classList.add("left");
-        warningIcon.textContent = "warning";
+    return plays.some(o =>
+        trunkDateToDay(o.end).getTime() === trunkDateToDay(currentPlay.end).getTime() &&
+        JSON.stringify(o.players) === currentPlayersFootPrint
+    );
+}
 
-        warning.appendChild(warningIcon);
-        warning.append("Une partie similaire a déjà été saisie (date/joueurs/scores identiques)");
+function addWarning() {
+    const divider = document.createElement("div");
+    divider.classList.add("divider");
 
-        row.appendChild(label);
-        row.appendChild(warning);
+    const row = document.createElement("div");
+    row.classList.add("row");
+    row.classList.add("extra-margin-top");
 
-        const modalContent = document.querySelector("#form-play .modal-content");
-        modalContent.appendChild(divider);
-        modalContent.appendChild(row);
-        modalContent.scrollTop = modalContent.scrollHeight;
-    }
-} catch (Exception) { };
+    const warning = document.createElement("div");
+    warning.classList.add("col");
+    warning.classList.add("s12");
+
+    const label = document.createElement("div");
+    label.textContent = "BGA2Myludo";
+    label.style = "padding-left:12px; font-size: .8rem; color: #9e9e9e;";
+
+    const warningIcon = document.createElement("i");
+    warningIcon.classList.add("material-icons");
+    warningIcon.classList.add("orange-text");
+    warningIcon.classList.add("middle");
+    warningIcon.classList.add("left");
+    warningIcon.textContent = "warning";
+
+    warning.appendChild(warningIcon);
+    warning.append("Une partie similaire a déjà été saisie (date/joueurs/scores identiques)");
+
+    row.appendChild(label);
+    row.appendChild(warning);
+
+    const modalContent = document.querySelector("#form-play .modal-content");
+    modalContent.appendChild(divider);
+    modalContent.appendChild(row);
+    modalContent.scrollTop = modalContent.scrollHeight;
+}
