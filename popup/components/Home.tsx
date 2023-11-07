fb068709d1c5f7b305d1344615baa9806172c831
Benjamin Bigot <benjamin.bigot@gmail.com>
2023-11-07 05:26:54 +0100

    change style on home

--- popup/components/Home.tsx
+++ popup/components/Home.tsx
@@ -1,5 +1,5 @@
 import EmailIcon from '@mui/icons-material/Email';
-import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import { List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
 import '~popup/index.scss';
 import DiscordIcon from './DiscordIcon';
 import GithubIcon from './GithubIcon';
@@ -8,28 +8,31 @@ export default function Home() {
     return (
         <div className="home">
             <div className="title">{chrome.i18n.getMessage("welcomeTitle")}</div>
-            <div className="message">{chrome.i18n.getMessage("welcomeMessageLine1")}</div>
-            <div className="message">{chrome.i18n.getMessage("welcomeMessageLine2")}</div>
+            <div className="message">
+                {chrome.i18n.getMessage("welcomeMessageLine1")}
+                <br />
+                {chrome.i18n.getMessage("welcomeMessageLine2")}
+            </div>
             <List dense>
                 <ListItem component="a" href="https://discord.gg/Xwp8VFVP37" target='_blank'>
                     <ListItemIcon>
                         <DiscordIcon />
                     </ListItemIcon>
-                    <ListItemText primary="Sur le discord de Myludo" />
+                    <ListItemText primary={<Typography className="message-list">{chrome.i18n.getMessage("contactByDiscord")}</Typography>} />
                 </ListItem>
 
                 <ListItem component="a" href="mailto:bga2myludo@gmail.com">
                     <ListItemIcon>
                         <EmailIcon />
                     </ListItemIcon>
-                    <ListItemText primary="Par Email à bga2myludo@gmail.com" />
+                    <ListItemText primary={<Typography className="message-list">{chrome.i18n.getMessage("contactByMail")}</Typography>} />
                 </ListItem>
 
                 <ListItem component="a" href="https://github.com/therealhnk/bga2myludo-web-extension/issues" target='_blank'>
                     <ListItemIcon>
                         <GithubIcon />
                     </ListItemIcon>
-                    <ListItemText primary="Via Github" />
+                    <ListItemText primary={<Typography className="message-list">{chrome.i18n.getMessage("contactByGithub")}</Typography>} />
                 </ListItem>
             </List>
         </div >
