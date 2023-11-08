e77111ca0eb5485e6c32c8cb813eeca529b4d3c1
Benjamin Bigot <benjamin.bigot@gmail.com>
2023-11-08 17:30:19 +0100

    finalise style on Configuration

--- popup/components/Configuration.tsx
+++ popup/components/Configuration.tsx
@@ -1,4 +1,4 @@
-import { FormControlLabel, FormGroup, Grid, Switch, TextField } from '@mui/material';
+import { Divider, List, ListItemButton, ListItemIcon, ListItemText, Switch, TextField } from '@mui/material';
 import { useCallback } from 'react';
 import type { Configuration as ConfigurationModel } from '~core/models/configuration';
 import '~popup/index.scss';
@@ -23,93 +23,76 @@ export default function Configuration({ configuration, onConfigurationUpdated }:
         onConfigurationUpdated({ ...configuration, [event.currentTarget.name]: value });
     }, [configuration, onConfigurationUpdated]);
 
+    const onClick = useCallback((name: string): void => {
+        onConfigurationUpdated({ ...configuration, [name]: !configuration[name] });
+    }, [configuration, onConfigurationUpdated]);
+
     return (
         <div className='configuration'>
             <div className="title">{chrome.i18n.getMessage("configurationTitle")}</div>
-            <div className="content">
-                <FormGroup>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <FormControlLabel
-                                control={
-                                    <Switch
-                                        checked={configuration.autoSubmit}
-                                        onChange={onChange}
-                                        name='autoSubmit'
-                                        size="small"
-                                    />
-                                }
-                                label={<span className='checkboxLabel'>{chrome.i18n.getMessage("configurationAutoSubmitLabel")}</span>}
-                                labelPlacement="start"
-                                title={chrome.i18n.getMessage("configurationAutoSubmitTitle")}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <FormControlLabel
-                                control={
-                                    <Switch
-                                        checked={configuration.excludeFromStatistics}
-                                        onChange={onChange}
-                                        name='excludeFromStatistics'
-                                        size="small"
-                                    />
-                                }
-                                label={<span className='checkboxLabel'>{chrome.i18n.getMessage("configurationExcludeFromStatisticsLabel")}</span>}
-                                labelPlacement="start"
-                                title={chrome.i18n.getMessage("configurationExcludeFromStatisticsTitle")}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <FormControlLabel
-                                control={
-                                    <Switch
-                                        checked={configuration.addTableLink}
-                                        onChange={onChange}
-                                        name='addTableLink'
-                                        size="small"
-                                    />
-                                }
-                                label={<span className='checkboxLabel'>{chrome.i18n.getMessage("configurationAddTableLinkLabel")}</span>}
-                                labelPlacement="start"
-                                title={chrome.i18n.getMessage("configurationAddTableLinkTitle")}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <FormControlLabel
-                                control={
-                                    <Switch
-                                        checked={configuration.fillPlace}
-                                        onChange={onChange}
-                                        name='fillPlace'
-                                        size="small"
-                                    />
-                                }
-                                label={<span className='checkboxLabel'>{chrome.i18n.getMessage("configurationfillPlaceLabel")}</span>}
-                                labelPlacement="start"
-                                title={chrome.i18n.getMessage("configurationfillPlaceTitle")}
+            <List dense>
+                <ListItemButton onClick={() => onClick("addTableLink")}>
+                    <ListItemIcon>
+                        <Switch checked={configuration.addTableLink} size='small' />
+                    </ListItemIcon>
+                    <ListItemText
+                        primary={<span className='configuration-item-primary'>{chrome.i18n.getMessage("configurationAddTableLinkLabel")}</span>}
+                        secondary={<span className='configuration-item-secondary'>{chrome.i18n.getMessage("configurationAddTableLinkTitle")}</span>}
+                    />
+                </ListItemButton>
 
-                            />
-                        </Grid>
-                        {configuration.fillPlace &&
-                            <>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        className='place'
-                                        value={configuration.place}
-                                        name='place'
-                                        onChange={onChange}
-                                        size="small"
-                                        label={chrome.i18n.getMessage("configurationPlaceLabel")}
-                                        title={chrome.i18n.getMessage("configurationPlaceTitle")}
-                                        variant='standard'
-                                        inputProps={{ style: { fontSize: 14 } }}
-                                    />
-                                </Grid>
-                            </>
-                        }
-                    </Grid>
-                </FormGroup>
-            </div >
-        </div >
+                <Divider />
+
+                <ListItemButton onClick={() => onClick("excludeFromStatistics")}>
+                    <ListItemIcon>
+                        <Switch checked={configuration.excludeFromStatistics} size='small' />
+                    </ListItemIcon>
+                    <ListItemText
+                        primary={<span className='configuration-item-primary'>{chrome.i18n.getMessage("configurationExcludeFromStatisticsLabel")}</span>}
+                        secondary={<span className='configuration-item-secondary'>{chrome.i18n.getMessage("configurationExcludeFromStatisticsTitle")}</span>}
+                    />
+                </ListItemButton>
+
+                <Divider />
+
+                <ListItemButton onClick={() => onClick("fillPlace")}>
+                    <ListItemIcon>
+                        <Switch checked={configuration.fillPlace} size='small' />
+                    </ListItemIcon>
+                    <ListItemText
+                        className='configuration-item'
+                        primary={<span className='configuration-item-primary'>{chrome.i18n.getMessage("configurationFillPlaceLabel")}</span>}
+                        secondary={<span className='configuration-item-secondary'>{chrome.i18n.getMessage("configurationFillPlaceTitle")}</span>}
+                    />
+                </ListItemButton>
+                {configuration.fillPlace &&
+                    <div>
+                        <TextField
+                            className='place'
+                            value={configuration.place}
+                            name='place'
+                            onChange={onChange}
+                            size="small"
+                            label={chrome.i18n.getMessage("configurationPlaceLabel")}
+                            title={chrome.i18n.getMessage("configurationPlaceTitle")}
+                            variant='standard'
+                            inputProps={{ style: { fontSize: 14 } }}
+                        />
+                    </div>
+                }
+
+                <Divider />
+
+                <ListItemButton onClick={() => onClick("autoSubmit")}>
+                    <ListItemIcon>
+                        <Switch checked={configuration.autoSubmit} size='small' />
+                    </ListItemIcon>
+                    <ListItemText
+                        primary={<span className='configuration-item-primary'>{chrome.i18n.getMessage("configurationAutoSubmitLabel")}</span>}
+                        secondary={<span className='configuration-item-secondary'>{chrome.i18n.getMessage("configurationAutoSubmitTitle")}</span>}
+                    />
+                </ListItemButton>
+            </List>
+        </div>
     )
 }
