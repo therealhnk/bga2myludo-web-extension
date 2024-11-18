57198ec0e179f7a89306127a071f614d64c2b9c2
Benjamin Bigot <benjamin.bigot@gmail.com>
2024-11-18 18:13:12 +0100

    Add link on last resultats page

--- contents/boardGameArena.ts
+++ contents/boardGameArena.ts
@@ -13,6 +13,7 @@ async function patch() {
     const regexTablePage = /^https:\/\/boardgamearena\.com\/table\?table=\d+#?$/;
     const regexpEndGamePage = /^https:\/\/boardgamearena\.com\/.*\btable=\d+.*$/;
     const regexpGameStatsPage = /^https:\/\/boardgamearena\.com\/gamestats/;
+    const regexpLastResultsPage = /^https:\/\/boardgamearena\.com\/player\?section=lastresults$/;
 
     if (document.getElementsByClassName(myLudoEltId).length === 0) {
         if (regexTablePage.test(window.location.href)) {
@@ -21,6 +22,8 @@ async function patch() {
             await patchEndGamePage();
         } else if (regexpGameStatsPage.test(window.location.href)) {
             await patchGameStatsPage();
+        } else if (regexpLastResultsPage.test(window.location.href)) {
+            await patchLastResultsPage();
         }
     }
 
@@ -69,6 +72,52 @@ async function patchEndGamePage() {
             }
         }
     }
+}
+
+async function patchLastResultsPage() {
+    let page = 1;
+
+    fetchAndFeedLastResultsPage(page);
+
+    document.querySelector("#board_seemore_r").addEventListener("click", () => {
+        fetchAndFeedLastResultsPage(++page);
+    });
+}
+
+function fetchAndFeedLastResultsPage(page: number) {
+    const timeout = setTimeout(() => {
+        // on vérifie si toutes les tables sont chargés et rendues
+        const rows = document.querySelectorAll('#boardposts_r .post .postcontent .postfooter:not(:has(a[href*="www.myludo.fr"]))');
+
+        if (!rows.length) return; // Vérifie si des lignes existent
+
+        rows.forEach((row) => {
+            const linkElement = row.closest('.post').querySelector('a[href*="/table?table="]');
+
+            if (linkElement) {
+                const tableUrl = linkElement.getAttribute('href');
+
+                // Extraire le paramètre table
+                const tableIdMatch = tableUrl.match(/table=(\d+)/);
+                if (tableIdMatch) {
+                    const tableId = tableIdMatch[1];
+
+                    const span = document.createElement("span");
+                    span.textContent = " • ";
+
+                    const link = document.createElement("a");
+                    link.href = `https://www.myludo.fr/#!/?bgatableid=${tableId}`;
+                    link.target = "_blank";
+                    link.textContent = "Enregistrer sur myLudo";
+
+                    span.appendChild(link);
+                    row.appendChild(span);
+                }
+            }
+        });
+
+        clearTimeout(timeout);
+    }, 125);
 }
 
 async function patchGameStatsPage() {
